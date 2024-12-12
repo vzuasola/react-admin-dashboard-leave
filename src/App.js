1f445f45fe4f9c003b1892f08e34e50e89fb4d31
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2024-12-12 19:47:41 +0800

    removed comment line

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
 import Request from "./scenes/request";
-// import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
